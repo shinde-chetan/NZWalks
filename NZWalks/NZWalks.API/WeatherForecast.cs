d0b083e8848da6c7ccbf95f19a55cbe7a9a4a815
Chetan Shinde <40253366+shinde-chetan@users.noreply.github.com>
2026-01-16 11:40:10 +0530

    Update WeatherForcast.cs
    Remove parameter

--- NZWalks/NZWalks.API/WeatherForecast.cs
+++ NZWalks/NZWalks.API/WeatherForecast.cs
@@ -9,7 +9,5 @@ namespace NZWalks.API
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
-
-        public boolean IsActive {get; set;}
     }
 }
