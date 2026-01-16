9dd4fc6598f187529212ae098a4a370f11e6df7c
Chetan Shinde <40253366+shinde-chetan@users.noreply.github.com>
2026-01-16 11:36:43 +0530

    Add IsActive property to WeatherForecast class
    Model updated with addition of a parameter

--- NZWalks/NZWalks.API/WeatherForecast.cs
+++ NZWalks/NZWalks.API/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace NZWalks.API
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public boolean IsActive {get; set;}
     }
 }
